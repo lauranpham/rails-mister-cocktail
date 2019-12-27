3faebe9ca3a170608c824cb73706f7c15e704835
Laura Pham <l.pham2311@gmail.com>
2019-12-27 16:12:46 +1100

    Changes for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.active_storage.service = :cloudinary
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -30,6 +29,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   # config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -48,7 +48,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
