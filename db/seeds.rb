b9d5fe1dd68e11509784afe9bea358ad0c5b7084
Laura Pham <l.pham2311@gmail.com>
2020-01-20 21:14:47 +0000

    Added more ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,20 @@ puts 'Cleaning database...'
 Ingredient.destroy_all
 
 puts 'Creating...'
+# Creating Ingredients
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "passionfruit")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "passoa")
+Ingredient.create(name: "lime juice")
+Ingredient.create(name: "sugar syrup")
+
+# Creating Cocktails
+Cocktail.create(name: "Pornstar Martini")
+Cocktail.create(name: "Whisky Sour")
+Cocktail.create(name: "Pina Colada")
+Cocktail.create(name: "Mojito")
 
 puts 'Finished!'
