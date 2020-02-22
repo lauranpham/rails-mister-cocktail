90048f6b51423c9daa821d87f2bc23ebde8c97a5
Laura Pham <l.pham2311@gmail.com>
2020-02-22 20:54:32 +0000

    Website front end stylings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# require "open-uri"
+require "open-uri"
 
 puts 'Cleaning database...'
 Dose.destroy_all
@@ -8,14 +8,12 @@ Ingredient.destroy_all
 
 puts 'Creating...'
 # Creating Ingredients
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "passionfruit")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "passoa")
-Ingredient.create(name: "lime juice")
-Ingredient.create(name: "sugar syrup")
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].sort_by {|ingredient| ingredient["strIngredient1"] }.each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
+end
 
 puts '..Cocktails'
 # Creating Cocktails
@@ -31,8 +29,5 @@ end
 # file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
 # article = Article.new(title: 'NES', body: "A great console")
 # article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
-
-puts '..Doses'
-# Creating Doses
 
 puts 'Finished!'
